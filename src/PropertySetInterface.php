96c603dad4bb4c5c926721ee4de92482710439a5
Malofeykin Andrey <malofeykin@platformasoft.ru>
2015-05-25 13:11:14 +0300

    Добавил composer.json

---
+++ src/PropertySetInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link https://github.com/old-town/old-town-propertyset
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace OldTown\PropertySet;
+
+/**
+ * Interface PropertySetInterface
+ *
+ * @package OldTown\PropertySet
+ */
+interface PropertySetInterface
+{
+
+}
