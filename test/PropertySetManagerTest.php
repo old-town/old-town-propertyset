68981c57f24f38a9861a0c9e67103a9bb963a12a
Malofeykin Andrey <malofeykin@platformasoft.ru>
2015-06-25 19:00:46 +0300

    sm-44 Структура модуля. Добавления модуля к travis-ci и coveralls

---
+++ test/PropertySetManagerTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link https://github.com/old-town/old-town-propertyset
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace OldTown\PropertySet\Test;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use OldTown\PropertySet\PropertySetManager;
+
+/**
+ * Class PropertySetManagerTest
+ *
+ * @package OldTown\PropertySet\Test
+ */
+class PropertySetManagerTest extends TestCase
+{
+
+    /**
+     * Проверка того что загружается PropertySetManager
+     *
+     * @return void
+     */
+    public function testCreatePropertySetManage()
+    {
+        $manager = new PropertySetManager();
+        static::assertInstanceOf(PropertySetManager::class,$manager);
+    }
+}
